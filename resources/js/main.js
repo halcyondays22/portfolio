24b16635099a81e5c5e84e9070fca67563e94316
halcyondays22 <tom.raley@gmail.com>
2018-11-18 14:48:53 -0600

    Add mobile nav close button

--- resources/js/main.js
+++ resources/js/main.js
@@ -7,3 +7,11 @@ $("#navbarSupportedContent").on('show.bs.collapse', function () {
         $("#navbarSupportedContent").collapse('hide');
     });
 });
+
+$("#mobile-nav-button").click(function() {
+    if (!$("#navbarSupportedContent").hasClass("show")) {
+        $("#mobile-nav-button").removeClass("fa-bars").addClass("fa-times");
+    } else  {
+        $("#mobile-nav-button").removeClass("fa-times").addClass("fa-bars");
+    }
+})
