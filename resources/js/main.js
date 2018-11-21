8e7ed5cc6bb10f6f8c12be55090e8bce80b848ad
halcyondays22 <tom.raley@gmail.com>
2018-11-20 20:33:00 -0600

    Fixed mobile navbar not closing on link click

--- resources/js/main.js
+++ resources/js/main.js
@@ -2,12 +2,16 @@ console.log("Connected!");
 
 AOS.init();
 
-$("#navbarSupportedContent").on('show.bs.collapse', function () {
-    $('.nav-link:not(#navbarDropdown)').click(function () {
-        $("#navbarSupportedContent").collapse('hide');
-    });
-});
+//Makes X change to hamburger on click of link inside of nav menu
+$(".nav-link").click(function() {
+    if (!$(".nav-link").hasClass("dropdown-toggle") && !$("#mobile-nav-button").hasClass("show")) {
+        $("#mobile-nav-button").removeClass("fa-bars").addClass("fa-times");
+    } else {
+        $("#mobile-nav-button").removeClass("fa-times").addClass("fa-bars");
+    }
+})
 
+//Makes hamburger change to X on click of hamburger button and back on click
 $("#mobile-nav-button").click(function() {
     if (!$("#navbarSupportedContent").hasClass("show")) {
         $("#mobile-nav-button").removeClass("fa-bars").addClass("fa-times");
@@ -15,3 +19,10 @@ $("#mobile-nav-button").click(function() {
         $("#mobile-nav-button").removeClass("fa-times").addClass("fa-bars");
     }
 })
+
+//Makes menu close when a link is clicked within menu
+$("#navbarSupportedContent").on('show.bs.collapse', function () {
+    $('.nav-link:not(#navbarDropdown)').click(function () {
+        $("#navbarSupportedContent").collapse('hide');
+    });
+});
