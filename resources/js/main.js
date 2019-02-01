16d92652f7e01cadcf0f424bba51f8b9350747b3
tom-raley <tom.raley@gmail.com>
2019-01-31 21:28:50 -0500

    Fix mobile dropdown hamburger toggle

--- resources/js/main.js
+++ resources/js/main.js
@@ -4,11 +4,7 @@ AOS.init();
 
 //Makes X change to hamburger on click of link inside of nav menu
 $(".nav-link:not(#navbarDropdown)").click(function() {
-    if ($("#mobile-nav-button").hasClass("fa-bars")) {
-        $("#mobile-nav-button").removeClass("fa-bars").addClass("fa-times");
-    } else  {
-        $("#mobile-nav-button").removeClass("fa-times").addClass("fa-bars");
-    }
+    $("#mobile-nav-button").toggleClass("is-active");
 })
 
 //Makes hamburger change to X on click of hamburger button and back on click
