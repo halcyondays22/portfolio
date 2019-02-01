fc99fa569c45663e5dc1a0e525292a707608be89
tom-raley <tom.raley@gmail.com>
2019-01-31 21:07:25 -0500

    Fix hamburger menu toggle

--- resources/js/main.js
+++ resources/js/main.js
@@ -13,12 +13,8 @@ $(".nav-link:not(#navbarDropdown)").click(function() {
 
 //Makes hamburger change to X on click of hamburger button and back on click
 $("#mobile-nav-button").click(function() {
-    if ($("#mobile-nav-button").hasClass("fa-bars")) {
-        $("#mobile-nav-button").removeClass("fa-bars").addClass("fa-times");
-    } else  {
-        $("#mobile-nav-button").removeClass("fa-times").addClass("fa-bars");
-    }
-})
+    $("#mobile-nav-button").toggleClass("is-active");
+});
 
 //Makes menu close when a link is clicked within menu
 $("#navbarSupportedContent").on('show.bs.collapse', function () {
